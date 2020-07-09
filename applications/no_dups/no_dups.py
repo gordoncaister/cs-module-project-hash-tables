82ae2512c078c5e82f5fc7620306b6924dbe0c30
Gordon Caister <gordoncaister@gmail.com>
2020-07-09 08:27:55 -0500

    no_dups complete

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,13 @@
 def no_dups(s):
     # Your code here
-
-
+    cache = {}
+    sarr = s.split()
+    print(sarr)
+    for word in sarr:
+        if word not in cache:
+            cache[word] = None
+    
+    return " ".join(cache)
 
 if __name__ == "__main__":
     print(no_dups(""))
